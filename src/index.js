b6c2730f202b2d926016729cfdedbd20e3554465
omiras <oscar.miras@gmail.com>
2022-07-04 12:14:04 +0200

    open api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 // *** ADD ***
 const championsRoutes = require('./v1/routes/championsRoutes');
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,4 +12,6 @@ app.use("/api/v1/champions", championsRoutes);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
+    V1SwaggerDocs(app, PORT);
+
 });
--- src/v1/routes/championsRoutes.js
+++ src/v1/routes/championsRoutes.js
@@ -3,9 +3,89 @@ const express = require('express');
 const championsControllers = require('../../controllers/championsControllers');
 
 const router = express.Router();
-
+/**
+ * @openapi
+ * /api/v1/champions:
+ *   get:
+ *     summary: Get all league of legends champions
+ *     tags:
+ *       - Champions
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                      
+ */
 router.get("/", championsControllers.getAllChampions);
+/**
+ * @openapi
+ * /api/v1/champions/{id}:
+ *   get:
+ *     summary: Get the league of legend champion identified by its id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         example: neeko
+ *     tags:
+ *       - Champions
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: object 
+ *                      
+ */
 router.get("/:id", championsControllers.getChampionById);
+
+/**
+ * @openapi
+ * /api/v1/champions/tags/{tag}:
+ *   get:
+ *     summary: Get the champions that belong to a specified tag
+ *     parameters:
+ *       - in: path
+ *         name: tag
+ *         schema:
+ *           type: string
+ *         required: true
+ *         example: Support
+ *     tags:
+ *       - Champions
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                      
+ */
 router.get("/tag/:tag", championsControllers.getChampionsByTag);
 
 module.exports = router;
